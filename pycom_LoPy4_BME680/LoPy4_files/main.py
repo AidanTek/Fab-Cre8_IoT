8f801ebafeb8a241dca9c111a5c9a1880aceed33
AidanTek <artaylor@cardiffmet.ac.uk>
2018-06-01 15:49:52 +0100

    LoPy4 BME680 example

---
+++ pycom_LoPy4_BME680/LoPy4_files/main.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+import bme680
+from bme680.i2c import I2CAdapter
+from machine import Pin
+import time
+
+def settimeout(duration): # can maybe get rid of this
+     pass
+
+# Config pin:
+configPin = Pin('P21', Pin.IN, Pin.PULL_UP)
+
+# Setup I2C and BME680: P9/G16=SDA, P10/G17=SCL
+i2c_dev = I2CAdapter(1, pins=('P9','P10'), baudrate=100000)
+sensor = bme680.BME680(i2c_device=i2c_dev)
+
+# These oversampling settings can be tweaked to
+# change the balance between accuracy and noise in
+# the data.
+sensor.set_humidity_oversample(bme680.OS_2X)
+sensor.set_pressure_oversample(bme680.OS_4X)
+sensor.set_temperature_oversample(bme680.OS_8X)
+sensor.set_filter(bme680.FILTER_SIZE_3)
+
+config = configPin()
+
+if config:
+    while True:
+        if sensor.get_sensor_data():
+
+            output = "{} C, {} hPa, {} RH, {} RES,".format(
+                sensor.data.temperature,
+                sensor.data.pressure,
+                sensor.data.humidity,
+                sensor.data.gas_resistance)
+
+            print(output)
+
+            time.sleep(5)
---
+++ pycom_LoPy4_BME680/readme.md
@@ -0,0 +1,5 @@
+# PyCom LoPy4 BME680 Sensor
+
+This mini project is an example of the BME680 sensor working on the PyCom LoPy4
+
+I use gkluoe's adaption of the Pimoroni BME680 library for the Raspberry Pi
